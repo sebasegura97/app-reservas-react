e7be17d36aba80da409ef0d61c14347b61b30505
sebasegura97 <sebasegura97@gmail.com>
2020-11-08 23:44:33 -0300

    no se que sucedio

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,8 @@
-import { Box } from "@material-ui/core"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { createMuiTheme } from "@material-ui/core/styles"
 import { ThemeProvider } from "@material-ui/styles"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
-// import Footer from "./Footer"
-import Header from "./Header"
+import React from "react"
 import Viewport from "./Viewport"
 
 const theme = createMuiTheme({
@@ -13,7 +10,7 @@ const theme = createMuiTheme({
     primary: {
       main: "#2063FF",
       light: "#7390B7",
-      dark: "#303481"
+      dark: "#303481",
     },
     secondary: {
       main: "#A10013",
--- src/pages/clientes.js
+++ src/pages/clientes.js
@@ -3,7 +3,7 @@ import React, { useState } from "react"
 import { ContextReserva } from "../components/Clientes/Reserva/ContextReserva"
 import Paso1 from "../components/Clientes/Reserva/Paso1"
 import Paso2 from "../components/Clientes/Reserva/Paso2"
-import Layout from "../components/Layout"
+import Layout from "../components/Layout.js"
 import SEO from "../components/SEO"
 
 const ClientesPage = () => {
@@ -14,22 +14,22 @@ const ClientesPage = () => {
   const steps = [<Paso1 />, <Paso2 />]
 
   return (
-    <ContextReserva.Provider value={{ context, setContext }}>
-      <Layout>
+    <Layout>
+      <ContextReserva.Provider value={{ context, setContext }}>
         <Box
           style={{
             background: "white",
           }}
           display="flex"
           alignItems="center"
-          paddingBottom={{ xs: 4, sm: 0}}
+          paddingBottom={{ xs: 4, sm: 0 }}
           component="main"
           minHeight="100vh"
         >
           {steps[context.step]}
         </Box>
-      </Layout>
-    </ContextReserva.Provider>
+      </ContextReserva.Provider>
+    </Layout>
   )
 }
 
