12d20e38f591bd268c21ca3b686405b6d374dc75
sebasegura97 <sebasegura97@gmail.com>
2020-11-08 23:29:36 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ["Avenir"],
+          families: ["Lato"],
         },
       },
     },
